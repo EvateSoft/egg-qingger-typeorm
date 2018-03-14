7887460ab88f68f45a9d46b5d91e94828acf82d7
shipfi <shipfi@testing>
2018-03-14 13:21:16 +0800

    build typeorm in current dic

--- dist/agent.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Qingger Corp.
- * User: jsspf
- * Date: 2018/1/24
- * Time: 0:37
- */
-declare const qinggerTypeorm: any;
--- dist/config/config.default.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/**
- * egg-qingger-typeorm default config
- * @member Config#qinggerTypeorm
- * @property {String} SOME_KEY - some description
- */
-exports.qinggerTypeorm = {};
