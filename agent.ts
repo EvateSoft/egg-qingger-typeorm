57e865628a0fac81b390d44aee15ca4e117d40f1
shipfi <shipfi@Thinkpad.T470>
2018-09-25 17:36:19 +0800

    User library to init qinggerTypeorm

--- agent.ts
+++ agent.ts
@@ -5,30 +5,10 @@
  * Time: 0:37
  */
 
-import {createConnection} from "typeorm";
-const assert = require("assert");
+const qinggerTypeorm = require("./lib/qinggerTypeorm");
 
 module.exports = function(agent) {
-    agent.addSingleton('qinggerTypeorm',createClient)
+   if(agent.config.qinggerTypeorm) {
+       return qinggerTypeorm(agent);
+   }
 };
-
-async function createClient(config,agent) {
-    assert(config.type && config.database ,
-        `[egg-typeorm] 'host: ${config.type}', 'database: ${config.database}' are required on config`);
-
-    assert((config.username && config.password && config.host)||config.replication,
-        `[egg-typeorm] 'username: ${config.username}','password: ${config.password}', 'host: ${config.host}' are required on config`);
-
-    const client = await createConnection(config);
-
-    // 做启动应用前的检查
-    let count = 1;
-    agent.beforeStart(async function startMysql() {
-        const rows = await client.manager.query('select now() as currentTime;');
-        agent.coreLogger.info(`[egg-typeorm] init instance success, rds currentTime: ${rows[0].currentTime}`);
-    });
-
-    agent.qinggerTypeorm = client;
-
-    return client;
-}
--- app.ts
+++ app.ts
@@ -4,34 +4,12 @@
  * Date: 2018/1/24
  * Time: 0:43
  */
-/// <reference path="./index.d.ts" />
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+
+const qinggerTypeorm = require("./lib/qinggerTypeorm");
 import {Application} from "egg";
-const assert = require("assert");
 
 module.exports = function (app:Application) {
-    app.addSingleton('qinggerTypeorm',createClient)
+    if (app.config.qinggerTypeorm) {
+        qinggerTypeorm(app);
+    }
 };
-
-async function createClient(config,app:Application) {
-    assert(config.type && config.database ,
-        `[egg-typeorm] 'host: ${config.type}', 'database: ${config.database}' are required on config`);
-
-    assert((config.username && config.password && config.host)||config.replication,
-        `[egg-typeorm] 'username: ${config.username}','password: ${config.password}', 'host: ${config.host}' are required on config`);
-
-    const client = await createConnection(config);
-
-    // 做启动应用前的检查
-    let count = 1;
-    app.beforeStart(async function startMysql() {
-        const rows = await client.manager.query('select now() as currentTime;');
-        app.coreLogger.info(`[egg-typeorm] init instance success, rds currentTime: ${rows[0].currentTime}`);
-    });
-
-
-    app.qinggerTypeorm = client;
-
-    return client;
-}
