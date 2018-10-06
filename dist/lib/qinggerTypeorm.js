3d2ba6433763f52cf201c96d586a33148a6914ab
shipfi <shipfi@Thinkpad.T470>
2018-10-06 17:17:18 +0800

    compiler to dist folder

---
+++ dist/lib/qinggerTypeorm.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/// <reference path="../index.d.ts" />
+require("reflect-metadata");
+const assert = require("assert");
+const typeorm_1 = require("typeorm");
+module.exports = async function (app) {
+    let config = app.config.qinggerTypeorm;
+    assert(config.type && config.database, `[egg-typeorm] 'host: ${config.type}', 'database: ${config.database}' are required on config`);
+    assert((config.username && config.password && config.host) || config.replication, `[egg-typeorm] 'username: ${config.username}','password: ${config.password}', 'host: ${config.host}' are required on config`);
+    let connection = await typeorm_1.createConnection(app.config.qinggerTypeorm);
+    app.qinggerTypeorm = connection;
+    let count = 1;
+    app.beforeStart(async function startMysql() {
+        const rows = await connection.manager.query('select 1 as column1;');
+        const index = count++;
+        app.coreLogger.info(`[egg-typeorm] instance[${index}] status OK, rds currentTime: ${rows[0].currentTime}`);
+    });
+    return connection;
+};
